385ed43b47b5346c76fc3dedcb938e4b9c066010
asmar.ali <asmar.ali@constell8.com>
2024-06-17 20:18:33 +0500

    added logos

---
+++ data/index.ts
@@ -0,0 +1,211 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Transforming a Vision into a Million-Dollar Product",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Ecommerce Store",
+    des: "Experience Seamless Shopping with an Expertly Crafted eCommerce Store.",
+    img: "/brands.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    link: "https://brands4cheap.com/",
+  },
+  {
+    id: 2,
+    title: "Fuel Treatment",
+    des: "An elegantly designed landing page for a business.",
+    img: "/fuel.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "https://fueltreatment.ng/",
+  },
+  {
+    id: 3,
+    title: "Landing Page",
+    des: "A fully responsive landing page developed with state of the art tech stack.",
+    img: "/hoobank.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "https://bankinghooapp.netlify.app",
+  },
+  {
+    id: 4,
+    title: "Nike Landing Page Clone",
+    des: "A fully responsive landing page",
+    img: "/nike.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "https://devnike.netlify.app",
+  },
+];
+
+export const testimonials = [
+  {
+    "quote": "Working with Asmar was a fantastic experience. Their clear communication, meticulous attention to detail, and ability to translate our vision into reality were invaluable. Their passion for their craft shines through in every interaction. We couldn't be happier with the final product and highly recommend them to anyone seeking a top-notch developer.",
+    "name": "Sarah Jones",
+    "title": "Marketing Manager, GreenTech Solutions"
+  },
+  {
+    "quote": "Asmar exceeded all expectations! Their expertise, efficiency, and commitment to quality were refreshing. They consistently went the extra mile to ensure our project's success. If you're looking for a reliable and results-oriented developer, look no further!",
+    "name": "David Lee",
+    "title": "CEO, Bloom & Co."
+  },
+  {
+    "quote": "Hiring Asmar was one of the best decisions we've made. Their creativity, problem-solving skills, and proactive approach were instrumental in bringing our project to life. They were a pleasure to collaborate with and truly understood our needs. Highly recommend!",
+    "name": "Emily Garcia",
+    "title": "Founder, The Wellness Collective"
+  },
+  {
+    "quote": "Asmar is a true asset! Their flexibility, adaptability, and willingness to learn proved invaluable throughout our collaboration. They consistently delivered high-quality work and readily embraced our feedback. We're impressed and grateful for their dedication!",
+    "name": "Omar Khan",
+    "title": "CTO, Skynet Inc."
+  },
+  {
+    "quote": "From the initial consultation to the final launch, Asmar provided exceptional service. Their responsiveness, transparency, and commitment to deadlines were commendable. We were thoroughly impressed with their expertise and wouldn't hesitate to recommend them!",
+    "name": "Isabella Hernandez",
+    "title": "Project Manager, EcoBuild"
+  }
+]
+export const companies = [
+  {
+    id: 1,
+    name: "Senarios",
+    img: "/senarios.jpg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "Constelle Business  Solutions",
+    img: "/constelle.jpeg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "Stealth Startup",
+    img: "/stealth.jpeg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Software Engineer",
+    desc: "Lead the development of a web-based platform developed using microservices based architecture.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Full Stack Developer",
+    desc: "Designed and developed a complete web based solution for a rapidly booming business.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Full Stack Developer",
+    desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Lead Frontend Developer",
+    desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+  },
+];
